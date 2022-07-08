a2b91785a12a0ae567ab0eb5e7c42499ce1a5f17
myselfshravan <shravanrevanna@gmail.com>
2022-07-08 13:57:57 +0530

    package update wit change

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Layout } from 'react-admin';
 import MyAppBar from './MyAppBar';
-import ResponsiveAppBar from './ResponsiveAppBar';
+// import ResponsiveAppBar from './ResponsiveAppBar';
 
 const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
 
